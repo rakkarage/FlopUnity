0100338ad2fd8854dfd9e48b8d036e99fd796b60
rakkarage <rakkarage@gmail.com>
2014-09-11 20:56:15 -0400

    x

--- Assets/Ease.cs
+++ Assets/Ease.cs
@@ -7,20 +7,20 @@ public static class Ease
 	public enum Type
 	{
 		Linear,
-        Hermite,
-        Sinerp,
-        Coserp,
+		Hermite,
+		Sinerp,
+		Coserp,
 		Spring
 	}
-    private delegate float EaseHandler(float start, float end, float t);
-    private static Dictionary<Type, EaseHandler> _types = new Dictionary<Type, EaseHandler>()
-    {
-        {Type.Linear, Mathf.Lerp},
-        {Type.Hermite, Hermite},
-        {Type.Sinerp, Sinerp},
-        {Type.Coserp, Coserp},
-        {Type.Spring, Spring}
-    };
+	private delegate float EaseHandler(float start, float end, float t);
+	private static Dictionary<Type, EaseHandler> _types = new Dictionary<Type, EaseHandler>()
+	{
+		{Type.Linear, Mathf.Lerp},
+		{Type.Hermite, Hermite},
+		{Type.Sinerp, Sinerp},
+		{Type.Coserp, Coserp},
+		{Type.Spring, Spring}
+	};
 	public static void Go(MonoBehaviour o, float start, float end, float t, UnityAction<float> callback, Type type)
 	{
 		o.StopAllCoroutines();
@@ -32,26 +32,26 @@ public static class Ease
 		while (i <= 1f)
 		{
 			i += Time.deltaTime / t;
-            callback(_types[type](start, end, i));
+			callback(_types[type](start, end, i));
 			yield return null;
 		}
 	}
-    public static float Hermite(float start, float end, float t)
-    {
-        return Mathf.Lerp(start, end, t * t * (3f - 2f * t));
-    }
-    public static float Sinerp(float start, float end, float t)
-    {
-        return Mathf.Lerp(start, end, Mathf.Sin(t * Mathf.PI * .5f));
-    }
-    public static float Coserp(float start, float end, float t)
-    {
-        return Mathf.Lerp(start, end, 1f - Mathf.Cos(t * Mathf.PI * .5f));
-    }
-    public static float Spring(float start, float end, float t)
-    {
-        t = Mathf.Clamp01(t);
-        t = (Mathf.Sin(t * Mathf.PI * (.2f + 2.5f * t * t * t)) * Mathf.Pow(1f - t, 2.2f) + t) * (1f + (1.2f * (1f - t)));
-        return start + (end - start) * t;
-    }
+	public static float Hermite(float start, float end, float t)
+	{
+		return Mathf.Lerp(start, end, t * t * (3f - 2f * t));
+	}
+	public static float Sinerp(float start, float end, float t)
+	{
+		return Mathf.Lerp(start, end, Mathf.Sin(t * Mathf.PI * .5f));
+	}
+	public static float Coserp(float start, float end, float t)
+	{
+		return Mathf.Lerp(start, end, 1f - Mathf.Cos(t * Mathf.PI * .5f));
+	}
+	public static float Spring(float start, float end, float t)
+	{
+		t = Mathf.Clamp01(t);
+		t = (Mathf.Sin(t * Mathf.PI * (.2f + 2.5f * t * t * t)) * Mathf.Pow(1f - t, 2.2f) + t) * (1f + (1.2f * (1f - t)));
+		return start + (end - start) * t;
+	}
 }
