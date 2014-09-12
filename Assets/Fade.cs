2f5fd854b24bf93c843b1c88c1653847f7e1cdff
rakkarage <rakkarage@gmail.com>
2014-09-11 21:20:21 -0400

    x

---
+++ Assets/Fade.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+public class Fade : MonoBehaviour
+{
+	public bool In = true;
+	public float Time = 3.33f;
+	private CanvasGroup _group;
+	private void Start()
+	{
+		float start = In ? 0f : 1f;
+		float end = In ? 1f : 0f;
+		_group = GetComponent<CanvasGroup>();
+		_group.alpha = start;
+		Ease.Go(GetComponent<Fade>(), start, end, Time, HandleFade, Ease.Type.Linear);
+	}
+	private void HandleFade(float alpha)
+	{
+		_group.alpha = alpha;
+	}
+}
