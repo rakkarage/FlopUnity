fdb8fe7f87091bfe2407e7923d6a3447085bfa67
rakkarage <rakkarage@gmail.com>
2014-11-14 20:19:02 -0500

    x

--- Assets/Ease.cs
+++ Assets/Ease.cs
@@ -59,54 +59,54 @@ public static class Ease
 }
 public static class Ease3
 {
-		public enum Type
+	public enum Type
+	{
+		Linear,
+		Hermite,
+		Sinerp,
+		Coserp,
+		Spring
+	}
+	private delegate Vector3 EaseHandler(Vector3 start, Vector3 end, float t);
+	private static Dictionary<Type, EaseHandler> _types = new Dictionary<Type, EaseHandler>()
+	{
+		{Type.Linear, Vector3.Lerp},
+		{Type.Hermite, Hermite},
+		{Type.Sinerp, Sinerp},
+		{Type.Coserp, Coserp},
+		{Type.Spring, Spring}
+	};
+	public static void Go(MonoBehaviour m, Vector3 start, Vector3 end, float t, Type type)
+	{
+		m.StopAllCoroutines();
+		m.StartCoroutine(GoCoroutine(m, start, end, t, type));
+	}
+	private static IEnumerator GoCoroutine(MonoBehaviour m, Vector3 start, Vector3 end, float t, Type type)
+	{
+		var i = 0f;
+		while (i <= 1f)
 		{
-				Linear,
-				Hermite,
-				Sinerp,
-				Coserp,
-				Spring
+			i += Time.deltaTime / t;
+			m.transform.localPosition = _types[type](start, end, i);
+			yield return null;
 		}
-		private delegate Vector3 EaseHandler(Vector3 start, Vector3 end, float t);
-		private static Dictionary<Type, EaseHandler> _types = new Dictionary<Type, EaseHandler>()
-		{
-				{Type.Linear, Vector3.Lerp},
-				{Type.Hermite, Hermite},
-				{Type.Sinerp, Sinerp},
-				{Type.Coserp, Coserp},
-				{Type.Spring, Spring}
-		};
-		public static void Go(MonoBehaviour m, Vector3 start, Vector3 end, float t, Type type)
-		{
-				m.StopAllCoroutines();
-				m.StartCoroutine(GoCoroutine(m, start, end, t, type));
-		}
-		private static IEnumerator GoCoroutine(MonoBehaviour m, Vector3 start, Vector3 end, float t, Type type)
-		{
-				var i = 0f;
-				while (i <= 1f)
-				{
-						i += Time.deltaTime / t;
-						m.transform.localPosition = _types[type](start, end, i);
-						yield return null;
-				}
-		}
-		private static Vector3 Hermite(Vector3 start, Vector3 end, float t)
-		{
-				return Vector3.Lerp(start, end, t * t * (3f - 2f * t));
-		}
-		private static Vector3 Sinerp(Vector3 start, Vector3 end, float t)
-		{
-				return Vector3.Lerp(start, end, Mathf.Sin(t * Mathf.PI * .5f));
-		}
-		private static Vector3 Coserp(Vector3 start, Vector3 end, float t)
-		{
-				return Vector3.Lerp(start, end, 1f - Mathf.Cos(t * Mathf.PI * .5f));
-		}
-		private static Vector3 Spring(Vector3 start, Vector3 end, float t)
-		{
-				t = Mathf.Clamp01(t);
-				t = (Mathf.Sin(t * Mathf.PI * (.2f + 2.5f * t * t * t)) * Mathf.Pow(1f - t, 2.2f) + t) * (1f + (1.2f * (1f - t)));
-				return start + (end - start) * t;
-		}
+	}
+	private static Vector3 Hermite(Vector3 start, Vector3 end, float t)
+	{
+		return Vector3.Lerp(start, end, t * t * (3f - 2f * t));
+	}
+	private static Vector3 Sinerp(Vector3 start, Vector3 end, float t)
+	{
+		return Vector3.Lerp(start, end, Mathf.Sin(t * Mathf.PI * .5f));
+	}
+	private static Vector3 Coserp(Vector3 start, Vector3 end, float t)
+	{
+		return Vector3.Lerp(start, end, 1f - Mathf.Cos(t * Mathf.PI * .5f));
+	}
+	private static Vector3 Spring(Vector3 start, Vector3 end, float t)
+	{
+		t = Mathf.Clamp01(t);
+		t = (Mathf.Sin(t * Mathf.PI * (.2f + 2.5f * t * t * t)) * Mathf.Pow(1f - t, 2.2f) + t) * (1f + (1.2f * (1f - t)));
+		return start + (end - start) * t;
+	}
 }
