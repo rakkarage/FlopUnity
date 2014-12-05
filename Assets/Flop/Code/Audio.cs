f4a28d47a208260b89926d8055bda8b3da6b83f9
rakkarage <rakkarage@gmail.com>
2014-12-05 00:15:39 -0500

    x

--- Assets/Flop/Code/Audio.cs
+++ Assets/Flop/Code/Audio.cs
@@ -5,6 +5,8 @@ namespace ca.HenrySoftware.Flop
 	{
 		public float VolumeMin = .5f;
 		public float VolumeMax = 1f;
+		public float VolumeLowMin = .3f;
+		public float VolumeLowMax = .6f;
 		public float PitchMin = .75f;
 		public float PitchMax = 1.25f;
 		public AudioClip Button0;
@@ -20,24 +22,32 @@ namespace ca.HenrySoftware.Flop
 		{
 			return Random.Range(VolumeMin, VolumeMax);
 		}
+		private float RandomVolumeLow()
+		{
+			return Random.Range(VolumeLowMin, VolumeLowMax);
+		}
 		private float RandomPitch()
 		{
 			return Random.Range(PitchMin, PitchMax);
 		}
 		private void RandomSound(AudioClip sound)
 		{
+			RandomSound(sound, false);
+		}
+		private void RandomSound(AudioClip sound, bool low)
+		{
 			var oldPitch = _source.pitch;
 			_source.pitch = RandomPitch();
-			_source.PlayOneShot(sound, RandomVolume());
+			_source.PlayOneShot(sound, low ? RandomVolumeLow() : RandomVolume());
 			_source.pitch = oldPitch;
 		}
 		public void PlayButton0()
 		{
-			RandomSound(Button0);
+			RandomSound(Button0, true);
 		}
 		public void PlayButton1()
 		{
-			RandomSound(Button1);
+			RandomSound(Button1, true);
 		}
 		public void PlayClick()
 		{
---
+++ Assets/Flop/Code/SoundOver.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+namespace ca.HenrySoftware.Flop
+{
+	public class SoundOver : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+	{
+		private Selectable _selectable;
+		private void Awake()
+		{
+			_selectable = GetComponent<Selectable>();
+		}
+		public void OnPointerEnter(PointerEventData e)
+		{
+			if (_selectable.interactable)
+				Audio.Instance.PlayButton1();
+	    }
+		public void OnPointerExit(PointerEventData e)
+		{
+			if (_selectable.interactable)
+				Audio.Instance.PlayButton0();
+		}
+	}
+}
