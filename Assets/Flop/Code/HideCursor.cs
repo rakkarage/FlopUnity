86beb3d0829f6b048fe4d300a9befdb9f573465b
rakkarage <rakkarage@gmail.com>
2014-12-10 08:35:17 -0500

    x

--- Assets/Flop/Code/HideCursor.cs
+++ Assets/Flop/Code/HideCursor.cs
@@ -6,6 +6,7 @@ namespace ca.HenrySoftware.Flop
 		void Start()
 		{
 #if !UNITY_EDITOR && (UNITY_IPHONE || UNITY_ANDROID)
+		    Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
 			Screen.showCursor = false;
 			Screen.lockCursor = true;
 #endif
