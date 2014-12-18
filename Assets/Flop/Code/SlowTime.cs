2375de2e56c9ca540820b30c8369530764c3de8d
rakkarage <rakkarage@gmail.com>
2014-12-17 22:19:51 -0500

    x

--- Assets/Flop/Code/SlowTime.cs
+++ Assets/Flop/Code/SlowTime.cs
@@ -1,12 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 namespace ca.HenrySoftware.Flop
 {
 	public class SlowTime : MonoBehaviour
 	{
+		[SerializeField]
+		private Image _status;
 		private bool _slow;
+		private void Start()
+		{
+			_status.color = Constants.StatusGreen;
+		}
 		public void Go()
 		{
 			Time.timeScale = (_slow = !_slow) ? .1f : 1f;
+			_status.color = _slow ? Constants.StatusRed : Constants.StatusGreen;
 		}
 	}
 }
