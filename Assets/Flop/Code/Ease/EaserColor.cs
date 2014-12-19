4c4eaf742368baf95fd8ca52d5d5f63e355529d1
rakkarage <rakkarage@gmail.com>
2014-12-19 18:26:22 -0500

    x

--- Assets/Flop/Code/Ease/EaserColor.cs
+++ Assets/Flop/Code/Ease/EaserColor.cs
@@ -1,14 +1,14 @@
 ﻿using UnityEngine;
 namespace ca.HenrySoftware.Flop
 {
-	public class EaserColor : Easer<Vector3>
+	public class EaserColor : Easer<Color>
 	{
 		public override void Go()
 		{
 			if (By)
-				Ease3.GoColorBy(this, Value, Time, null, Complete.Invoke, Type, Delay, Repeat, PingPong, RealTime);
+				Ease3.GoColorBy(this, Value.GetVector(), Time, null, Complete.Invoke, Type, Delay, Repeat, PingPong, RealTime);
 			else
-				Ease3.GoColorTo(this, Value, Time, null, Complete.Invoke, Type, Delay, Repeat, PingPong, RealTime);
+				Ease3.GoColorTo(this, Value.GetVector(), Time, null, Complete.Invoke, Type, Delay, Repeat, PingPong, RealTime);
 		}
 	}
 }
