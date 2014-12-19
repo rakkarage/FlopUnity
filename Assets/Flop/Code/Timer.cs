5f5450f1d3b3b4543892c42c20d0183c8ac7cf77
rakkarage <rakkarage@gmail.com>
2014-12-19 13:10:16 -0500

    x

--- Assets/Flop/Code/Timer.cs
+++ Assets/Flop/Code/Timer.cs
@@ -5,13 +5,13 @@ namespace ca.HenrySoftware.Flop
 	public class Timer : MonoBehaviour
 	{
 		[SerializeField]
-		private Image _statusSlow;
+		private Image _slowStatus;
 		private bool _slow;
 		[SerializeField]
-		private Image _statusPause;
+		private Image _pauseStatus;
 		private bool _pause;
 		[SerializeField]
-		private Image _statusFast;
+		private Image _fastStatus;
 		private bool _fast;
 		private readonly Color _colorOff = Color.white.SetAlpha(.75f);
 		private readonly Color _colorOn = Constants.ButtonBlue.SetAlpha(.75f);
@@ -19,46 +19,46 @@ namespace ca.HenrySoftware.Flop
 		private const float Time = .333f;
         private void Start()
 		{
-			_statusSlow.color = _colorOff;
-			_statusPause.color = _colorOff;
-			_statusFast.color = _colorOff;
+			_slowStatus.color = _colorOff;
+			_pauseStatus.color = _colorOff;
+			_fastStatus.color = _colorOff;
 		}
 		public void Slow()
 		{
 			_pause = false;
-			_statusPause.color = _colorOff;
+			_pauseStatus.color = _colorOff;
 			_fast = false;
-			_statusFast.color = _colorOff;
+			_fastStatus.color = _colorOff;
 			UnityEngine.Time.timeScale = (_slow = !_slow) ? .1f : 1f;
-			_statusSlow.StopAllCoroutines();
-			Ease3.GoColorTo(_statusSlow, (_slow ? _colorOn : _colorOff).GetVector(), Time, null, null, EaseType.SineInOut, 0f, 1, false, true);
-			Ease3.GoScaleTo(_statusSlow, _scaleTo, Time, null, () =>
+			_slowStatus.StopAllCoroutines();
+			Ease3.GoColorTo(_slowStatus, (_slow ? _colorOn : _colorOff).GetVector(), Time, null, null, EaseType.SineInOut, 0f, 1, false, true);
+			Ease3.GoScaleTo(_slowStatus, _scaleTo, Time, null, () =>
 			{
-				Ease3.GoScaleTo(_statusSlow, Vector3.one, Time, null, null, EaseType.BackInOut, 0f, 1, false, true);
+				Ease3.GoScaleTo(_slowStatus, Vector3.one, Time, null, null, EaseType.BackInOut, 0f, 1, false, true);
 			}, EaseType.BackInOut, 0f, 1, false, true);
 		}
 		public void Pause()
 		{
 			UnityEngine.Time.timeScale = (_pause = !_pause) ? 0f : _slow ? .1f : _fast ? 2f : 1f;
-			_statusPause.StopAllCoroutines();
-			Ease3.GoColorTo(_statusPause, (_pause ? _colorOn : _colorOff).GetVector(), Time, null, null, EaseType.SineInOut, 0f, 1, false, true);
-			Ease3.GoScaleTo(_statusPause, _scaleTo, Time, null, () =>
+			_pauseStatus.StopAllCoroutines();
+			Ease3.GoColorTo(_pauseStatus, (_pause ? _colorOn : _colorOff).GetVector(), Time, null, null, EaseType.SineInOut, 0f, 1, false, true);
+			Ease3.GoScaleTo(_pauseStatus, _scaleTo, Time, null, () =>
 			{
-				Ease3.GoScaleTo(_statusPause, Vector3.one, Time, null, null, EaseType.BackInOut, 0f, 1, false, true);
+				Ease3.GoScaleTo(_pauseStatus, Vector3.one, Time, null, null, EaseType.BackInOut, 0f, 1, false, true);
 			}, EaseType.BackInOut, 0f, 1, false, true);
 		}
 		public void Fast()
 		{
 			_slow = false;
-			_statusSlow.color = _colorOff;
+			_slowStatus.color = _colorOff;
 			_pause = false;
-			_statusPause.color = _colorOff;
+			_pauseStatus.color = _colorOff;
 			UnityEngine.Time.timeScale = (_fast = !_fast) ? 2f : 1f;
-			_statusFast.StopAllCoroutines();
-			Ease3.GoColorTo(_statusFast, (_fast ? _colorOn : _colorOff).GetVector(), Time, null, null, EaseType.SineInOut, 0f, 1, false, true);
-			Ease3.GoScaleTo(_statusFast, _scaleTo, Time, null, () =>
+			_fastStatus.StopAllCoroutines();
+			Ease3.GoColorTo(_fastStatus, (_fast ? _colorOn : _colorOff).GetVector(), Time, null, null, EaseType.SineInOut, 0f, 1, false, true);
+			Ease3.GoScaleTo(_fastStatus, _scaleTo, Time, null, () =>
 			{
-				Ease3.GoScaleTo(_statusFast, Vector3.one, Time, null, null, EaseType.BackInOut, 0f, 1, false, true);
+				Ease3.GoScaleTo(_fastStatus, Vector3.one, Time, null, null, EaseType.BackInOut, 0f, 1, false, true);
 			}, EaseType.BackInOut, 0f, 1, false, true);
 		}
 	}
