dcaeb436bce9677b700cf65602d96c9ca3f36b09
rakkarage <rakkarage@gmail.com>
2015-02-08 21:11:58 -0500

    x

---
+++ Assets/Flop/Code/FlowBasic.cs
@@ -0,0 +1,23 @@
+﻿using ca.HenrySoftware.Flop;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+public class FlowBasic : Flow<int>
+{
+    private readonly List<int> _data = Enumerable.Range(1, 10).ToList();
+    protected override List<int> Data
+    {
+        get { return _data; }
+    }
+    protected override void Apply(GameObject o, int d)
+    {
+        Debug.Log(o);
+        var data = ((char)d).ToString();
+        foreach (var text in o.GetComponentsInChildren<Text>(true))
+            text.text = data;
+    }
+   	protected override void UpdateCurrent(int current)
+    {
+    }
+}
